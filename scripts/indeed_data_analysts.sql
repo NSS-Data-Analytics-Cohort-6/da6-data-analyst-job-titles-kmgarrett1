19ae1624afe2f20161c100fd4884a6be9e95670f
Katie Garrett <katiegarrett@katies-air.lan>
2022-02-19 10:21:17 -0600

    Saturday V2

--- scripts/indeed_data_analysts.sql
+++ scripts/indeed_data_analysts.sql
@@ -36,9 +36,9 @@ FROM data_analyst_jobs
 WHERE review_count >= 500 AND review_count <=1000;
 answer: 151*/
 
-/*6.Show the average star rating for companies in each state. The output should show the state as state and the average rating for the state as avg_rating. Which state shows the highest average rating?*/
+/*6.Show the average star rating for companies in each state. The output should show the state as state and the average rating for the state as avg_rating. Which state shows the highest average rating?
 
-/*SELECT location, AVG(star_rating) AS avg_rating
+SELECT location, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
 WHERE location IS NOT NULL
 AND star_rating IS NOT NULL
@@ -47,30 +47,30 @@ ORDER BY avg_rating DESC;
 answer: NE */
 
 
-/*7.Select unique job titles from the data_analyst_jobs table. How many are there?*/
+/*7.Select unique job titles from the data_analyst_jobs table. How many are there?
 
-/*SELECT COUNT(DISTINCT title)
+SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs;
 answer:881*/
 
-/*8.How many unique job titles are there for California companies?*/
-/*SELECT COUNT(DISTINCT title)
+/*8.How many unique job titles are there for California companies?
+SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
 WHERE location = 'CA';
 answer: 230*/
 
-/*9.Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?*/
+/*9.Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
-/*SELECT COUNT(DISTINCT company), company, AVG(star_rating)
+SELECT COUNT(DISTINCT company), company, AVG(star_rating)
 FROM data_analyst_jobs
 WHERE review_count > 5000
 AND company IS NOT NULL
 GROUP BY company;
 answer: 40 */
 
-/*10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?*/
+/*10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
-/*SELECT DISTINCT company, COUNT(*) AVG(star_rating)
+SELECT DISTINCT company, COUNT(*) AVG(star_rating)
 FROM data_analyst_jobs
 WHERE review_count > 5000
 AND company IS NOT NULL 
@@ -85,33 +85,31 @@ Answer:
 "Nike"	4.1999998090000000
 "Unilever"	4.1999998090000000 */
 
-/*11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?*/
+/*11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
 
-/*SELECT COUNT(title)
+SELECT COUNT(title)
 FROM data_analyst_jobs
 WHERE title LIKE '%Analyst%';
 answer: 1636 */
 
 
-/*12.How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?*/
+/*12.How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
-/*SELECT COUNT(title)
+SELECT COUNT(title)
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst%' 
-AND title NOT LIKE '%Analytics%';
-answer: 39 */
+WHERE title NOT ILIKE '%Analy%'
+answer: 4 */
 
-/*BONUS: You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.*/
-
-/*Disregard any postings where the domain is NULL.
+/*BONUS: You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.
+Disregard any postings where the domain is NULL.
 Order your results so that the domain with the greatest number of hard to fill jobs is at the top.
-Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4*/
+Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4
 
-/*SELECT COUNT(domain), domain
+SELECT COUNT(domain), domain
 FROM data_analyst_jobs
 WHERE skill LIKE '%SQL%'
 	AND days_since_posting > 21
-	And domain IS NOT NULL
+	AND domain IS NOT NULL
 GROUP BY domain
 ORDER BY count DESC;
 
